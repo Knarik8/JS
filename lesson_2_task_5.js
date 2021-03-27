b9f67128d3e2c67a6d4be3401c6b54ec211815c7
Knarik8 <k_armina@mail.ru>
2021-03-28 01:41:37 +0300

    Урок_2

---
+++ lesson_2_task_5.js
@@ -0,0 +1,22 @@
+/*5. Реализовать основные 4 арифметические операции в виде функций с двумя параметрами.
+Обязательно использовать оператор return.*/
+
+function addition(a, b) {
+    return a + b;
+}
+
+function subtraction(a, b) {
+    return a - b;
+}
+
+function multiplication(a, b) {
+    return a * b;
+}
+
+function division(a, b) {
+    return a / b;
+}
+alert(addition(10, 5))
+alert(subtraction(10, 5))
+alert(multiplication(10, 5))
+alert(division(10, 5))
